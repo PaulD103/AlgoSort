57b59dd66546f28ab02a6f528b38e1170dd6f34e
PaulD103 <pauldunz@icloud.com>
2021-05-31 17:14:21 +0200

    mocha index.test.js file

---
+++ index.test.js
@@ -0,0 +1,14 @@
+const algosort = require("./index.js");
+const assert = require('assert').strict;
+
+describe("sorting and output test", function() {
+    it("should be able to output the sorted array with all different output options", function() {
+        var sortedNumbers = algosort.algosort({
+    		algorithm: 'bubblesort',
+    		numbers: [1, 3, 2, 7, 3, 9, 5, 6],
+    		output: 'afterEachNumber'
+  	});
+
+	console.log(sortedNumbers);
+    });
+});
